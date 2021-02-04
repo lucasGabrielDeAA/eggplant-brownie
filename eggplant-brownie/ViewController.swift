49845b1965c368634ca0e804d17a4b6d82e0c8d1
lucasGabrielDeAA <lg.technus@gmail.com>
2021-02-04 10:54:07 -0300

    Initial Commit

---
+++ eggplant-brownie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  eggplant-brownie
+//
+//  Created by Lucas Gabriel de Araújo Assis on 04/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
